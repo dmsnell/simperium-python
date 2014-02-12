c31f9e3b9cd59c64d066e96792e7d3f063bddc78
Dennis Snell <dmsnell@purdue.edu>
2014-02-11 20:29:01 -0500

    Binary API CLI Test: Removed redundant class and fixed -k parameter.
    Previously, the parameters from argparse were passed into an empty
    class, which was unneccessary. That class has been removed and the
    calling code simplified.
    
    The action='store_const' wasn't appropriate for -k, --key, whereas
    if you passed a parameter for -k, the script wouldn't recognize it.
    Now, the action is the default and const=None has been replaced by
    a more suitable default=None. The code now accepts -k passed from
    the command line.

--- simperium/test/storeBinaryObject.py
+++ simperium/test/storeBinaryObject.py
@@ -4,24 +4,19 @@ import sys
 
 from simperium import core
 
-class Args(object):
-	pass
-
-def get_args():
+def parse_args():
 	parser = argparse.ArgumentParser(description='Upload a file to a Simperium binary bucket.')
 	parser.add_argument('-a', '--app', help='Simperium App ID', required=True)
 	parser.add_argument('-b', '--bucket', help='Bucket to open', required=True)
 	parser.add_argument('-f', '--file', help='Filename for data to upload', type=argparse.FileType('r'), default=sys.stdin)
 	parser.add_argument('-i', '--item', help='Item name for upload', default='Unsorted_Files')
-	parser.add_argument('-k', '--key', help='Key name for upload', action='store_const', const=None)
+	parser.add_argument('-k', '--key', help='Key name for upload', default=None)
 	parser.add_argument('-t', '--token', help='Simperium user access token', required=True)
 
-	return parser
+	return parser.parse_args()
 
 def main():
-	args = Args()
-	parser = get_args()
-	parser.parse_args(namespace=args)
+	args = parse_args()
 
 	bucket = core.Bucket(args.app, args.token, args.bucket)
 
